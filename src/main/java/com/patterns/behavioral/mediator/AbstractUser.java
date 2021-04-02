e46b30c95bbc4758617ddc9a9d7bdc2d60a7db2b
scarrasco85 <chechucarrasco@gmail.com>
2021-04-02 12:19:48 +0200

    Resolving not access packages from Jenkins

--- src/main/java/com/patterns/behavioral/mediator/AbstractUser.java
+++ src/main/java/com/patterns/behavioral/mediator/AbstractUser.java
@@ -5,7 +5,7 @@ public abstract class AbstractUser {
     protected ChatMediator mediator;
     protected String name;
 
-    public AbstractUser(ChatMediator mediator, String name) {
+    protected AbstractUser(ChatMediator mediator, String name) {
         this.mediator = mediator;
         this.name = name;
         this.mediator.addUser(this);
